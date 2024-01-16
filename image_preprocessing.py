227fee6b402062367570cb70c3f15c42e9c042ef
JuliaGraf <juliacarolin.graf@web.de>
2024-01-16 15:46:43 +0100

    Add docstrings and integrate VGG16

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,28 +1,34 @@
+import argparse
+
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 import pydicom
 from skimage import img_as_ubyte
 from skimage.transform import resize
 from PIL import Image
 from cmap import Colormap
+import tensorflow as tf
 
 
 def remove_artefacts(img):
-    """Remove artefacts from the image.
+    """Removes all artefacts from the image.
 
-        img: numpy array, grayscale image with values between 0 and 255.
-        output: numpy array, grayscale image with values between 0 and 255.
+            Parameters:
+                img (np.array): Pixel array of the image (grayscale, values in range [0, 255])
+
+            Returns:
+                A numpy array representing the processed image (grayscale, values in range [0, 255])
     """
-    # Resize the image to 224px x 224px
-    resized_img = resize(img, output_shape=(224, 224), anti_aliasing=False, order=0)
+
     # Remove borders (5px)
     rectangle = np.ones(shape=(224, 224))
-    rectangular_mask = cv2.rectangle(rectangle, pt1=(0, 0), pt2=(resized_img.shape[1], resized_img.shape[0]),
+    rectangular_mask = cv2.rectangle(rectangle, pt1=(0, 0), pt2=(img.shape[1], img.shape[0]),
                                      color=(0, 0, 0), thickness=5)
     rectangular_mask = img_as_ubyte(rectangular_mask)
-    remove_borders = cv2.bitwise_and(rectangular_mask, resized_img)
+    remove_borders = cv2.bitwise_and(rectangular_mask, img)
     # Morphological Opening
+    # TODO: Experiment with the threshold to obtain the best outcomes
     binary_mask = cv2.threshold(remove_borders, thresh=25, maxval=255, type=cv2.THRESH_BINARY)[1]
     kernel = np.ones((20, 20), np.uint8)
     opening = cv2.morphologyEx(binary_mask, op=cv2.MORPH_OPEN, kernel=kernel)
@@ -37,6 +43,15 @@ def remove_artefacts(img):
 
 
 def image_enhancement(img):
+    """Applies Gamma Correction and CLAHE to enhance the quality of the image.
+
+            Parameters:
+                img (np.array): Pixel array of the image (grayscale, values in range [0, 255])
+
+            Returns:
+                A numpy array representing the processed image (grayscale, values in range [0, 255])
+    """
+
     # Scale values to [0,1]
     img = img / 255
     # Apply Gamma Correction
@@ -50,21 +65,25 @@ def image_enhancement(img):
     img = clahe.apply(img)
     img = clahe.apply(img)
     # Green Fire Blue
-    cmap = Colormap('imagej:GreenFireBlue').to_mpl()
-    sm = plt.cm.ScalarMappable(cmap=cmap)
-    color_range = sm.to_rgba(np.linspace(0, 1, 256), bytes=True)[:, 2::-1]
-    color_range = color_range.reshape(256, 1, 3)
-    img = cv2.applyColorMap(img, color_range)
+    # cmap = Colormap('imagej:GreenFireBlue').to_mpl()
+    # sm = plt.cm.ScalarMappable(cmap=cmap)
+    # color_range = sm.to_rgba(np.linspace(0, 1, 256), bytes=True)[:, 2::-1]
+    # color_range = color_range.reshape(256, 1, 3)
+    # img = cv2.applyColorMap(img, color_range)
 
     return img
 
 
 def dicom_to_pixel_array(path):
-    """Convert DICOM file to numpy array.
+    """Converts a DICOM file to numpy array.
 
-        path: path to DICOM file.
-        output: numpy array, grayscale pixels with values between 0 and 255.
+            Parameters:
+                path (str): The path to the DICOM file.
+
+            Returns:
+                A numpy array representing the processed image (grayscale, values in range [0, 255])
     """
+
     dicom = pydicom.dcmread(path)
     data = dicom.pixel_array
     # Convert the scale to [0,255]
@@ -74,23 +93,46 @@ def dicom_to_pixel_array(path):
     return data
 
 
-def preprocess_image(path, dicom=True):
-    """Apply all preprocessing steps to the image.
+def preprocess_image(path, mode, model, dicom=False):
+    """Applies all pre-processing steps to the image.
 
-        path: path to the DICOM file
-        output: numpy array, grayscale pixels with values between 0 and 255
+            Parameters:
+                path (str): The path to the DICOM file.
+                dicom (bool): Whether the images are in DICOM format (default: False).
+                mode (str): One of 'self-written', "tf".
+                    - self-written: will apply our own self-written pre-processing steps.
+                    - tf: will apply the preprocessing function, that the tf.keras.applications package provides.
+                model (str): One of 'ResNet50', 'VGG16'.
+                    - ResNet50: If mode is 'tf', will apply tf.keras.applications.resnet50.preprocess_input().
+                    - VGG16: If mode is 'tf', will apply tf.keras.applications.vgg16.preprocess_input().
+
+            Returns:
+                A numpy array representing the processed image (grayscale, values in range [0, 255])
     """
+
     if dicom:
-        data = dicom_to_pixel_array(path)
+        img_data = dicom_to_pixel_array(path)
     else:
-        data = Image.open(path)
-        data = np.array(data)
+        img_data = Image.open(path)
+        img_data = np.array(img_data)
 
-    data = remove_artefacts(data)
-    # Maybe implement the remove_line step here!
-    data = image_enhancement(data)
-    # Each color channel is zero-centered (range[-1,1])
-    # TODO: Maybe try to copy _preprocess_numpy_input() from the keras library github repo or without any scaling!
-    # data = (data/127.5) - 1
+    # Resize the image to 224px x 224px
+    img_data = resize(img_data, output_shape=(224, 224), anti_aliasing=False, order=0)
 
-    return data
+    if mode == 'self-written':
+        img_data = remove_artefacts(img_data)
+        img_data = image_enhancement(img_data)
+        # Since the ImageNet weights are trained on RGB images, convert the grayscale image to RGB
+        img_data = cv2.cvtColor(img_data, cv2.COLOR_GRAY2RGB)
+        # TODO: maybe there is a problem here because the values are not zero-centered with respect to the ImageNet
+        #  dataset!
+        # Each color channel is zero-centered (range[-1,1])
+        # img_data = (img_data/127.5) - 1
+        return img_data
+    else:
+        # mode == 'tf'
+        if model == 'ResNet50':
+            return tf.keras.applications.resnet50.preprocess_input(img_data)
+        else:
+            # model == 'VGG16'
+            return tf.keras.applications.vgg16.preprocess_input(img_data)

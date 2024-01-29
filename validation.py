87794b22c805fc551165864dda024a5c1b859f1e
JuliaGraf <juliacarolin.graf@web.de>
2024-01-29 14:10:23 +0100

    Fix error in validation and add support for ResNet

--- validation.py
+++ validation.py
@@ -1,88 +1,75 @@
-import cv2
-import csv
-import os
-from itertools import islice
-from sklearn.model_selection import train_test_split
-import numpy as np
-from keras.applications.vgg16 import VGG16
-from keras import layers, optimizers
-from keras.layers import Flatten
-from keras.models import Model
-import tensorflow as tf
 import matplotlib.pyplot as plt
-import scikitplot as skplt
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.preprocessing.image import load_img
-from sklearn.datasets import load_sample_image
-from sklearn.metrics import classification_report
-from sklearn.model_selection import cross_val_score
 from sklearn import metrics
-from PIL import Image
-from PIL import ImageOps, ImageFilter
-from keras.preprocessing.image import ImageDataGenerator
-from skimage import exposure
-import random
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import EarlyStopping
 from statistics import *
-# import BreastCancerClassification_VGG16
 import Breast_Cancer_Classification_ResNet50
-
-"""validation of model
-
-       Parameters:
-           images (np.array): Array of pixel array of images.
-           labels (np.array): Array of corresponding labels.
-           mode (string): model that should get validated ["VGG16","ResNet50"]
-           k (int): number of different trainings and test.
+import Breast_Cancer_Classification_VGG16
 
 
-       Returns:
-           A numpy array with the mean auc and accuracy of the model in the different runs with random train_test_splits
+def validation(images_train_val, labels_train_val, images_test, labels_test, model_type, k):
+    """Validate the model and plot the ROC curve.
 
-"""
-def validation(images_trainval, labels_trainval, images_test, labels_test, mode, k):
-    
-    #images_trainval = images_trainval[0:100]
-    #labels_trainval = labels_trainval[0:100]
+           Parameters:
+               images_train_val (np.array): Contains the pixel arrays of the training and validation images.
+               images_test (np.array): Contains the pixel arrays of the test images.
+               labels_train_val (np.array): Array of labels for the training and validation images.
+               labels_test (np.array): Array of labels for the test images.
+               model_type (str): Name of the model to be validated. One of "VGG16","ResNet50".
+               k (int): number of different training runs.
+
+           Returns:
+                A numpy array with the mean AUC and accuracy of the model in the different runs with random
+                train_test_splits.
+    """
 
     results_auc = []
     results_acc = []
     
     for i in range(k):
-        
-        
-        if mode == "VGG16":
-            pass
-            #model = BreastCancerClassification_VGG16.vgg16()
-        elif mode == "ResNet50":
+        assert model_type in ['VGG16', 'ResNet50']
+        if model_type == "VGG16":
+            model = Breast_Cancer_Classification_VGG16.vgg16()
+        else:
+            # mode == 'ResNet50'
             model = Breast_Cancer_Classification_ResNet50.resnet50()
 
-        X_train, X_val, Y_train, y_val = train_test_split(images_trainval, labels_trainval, test_size=0.125,
+        X_train, X_val, Y_train, y_val = train_test_split(images_train_val, labels_train_val, test_size=0.125,
                                                           random_state=None)
         
         callback = EarlyStopping(monitor='val_loss', mode='auto', patience=3, verbose=1, start_from_epoch=5,
                                  restore_best_weights=True)
 
+        if model_type == "VGG16":
+            batch_size = 1
+        else:
+            # mode == 'ResNet50'
+            batch_size = 32
+
         model.fit(
             X_train,
             Y_train,
-            epochs=2,
+            epochs=50,
+            batch_size=batch_size,
             validation_data=(X_val, y_val),
             callbacks=[callback],
             verbose=2
         )
 
         predictions = model.predict(images_test)
-        if mode == "ResNet50":
+
+        if model_type == "ResNet50":
             # Convert multilabel-indicator format to binary labels for the roc function
-            predictions = [0 if x[0] > x[1] else 1 for x in predictions]
+            # to_categorical([0, 1], 2) = [[1. 0.], [0. 1.]]
+            # Store the probability estimates of the positive class
+            predictions = [x[1] for x in predictions]
+            # Convert one-hot encoded labels back to integer labels
             labels_test_binary = [0 if x[0] == 1 else 1 for x in labels_test]
         else:
             predictions = predictions.reshape(-1)
             labels_test_binary = labels_test
+
         fpr, tpr, thresholds = metrics.roc_curve(labels_test_binary, predictions)
-        print(fpr)
-        print(tpr)
         label = "model " + str(i)
         plt.plot(fpr, tpr, label=label)
 
